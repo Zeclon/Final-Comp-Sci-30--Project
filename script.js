c31ba84dd56ff51003a720f78c5378d25871f221
Marc Bucknor <zeclon12@gmail.com>
2026-05-14 08:32:45 -0600

    upd

---
+++ script.js
@@ -0,0 +1,16 @@
+class Card {
+    constructor(color, rank, type, id) {
+        this.color = color;
+        this.rank = rank;
+        this.type = type;
+        this.id = id;
+    } // constructor
+
+    display() {
+
+    } // display
+
+} // end Card class
+
+
+              
